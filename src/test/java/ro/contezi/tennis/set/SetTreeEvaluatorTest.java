ed0aa35ae7a8375807cd8df80ffccb21c05323f7
Catalin Lazar <lazcatluc@gmail.com>
2017-07-12 20:50:34 +0300

    Change equal probability test to be more meaningful

--- src/test/java/ro/contezi/tennis/set/SetTreeEvaluatorTest.java
+++ src/test/java/ro/contezi/tennis/set/SetTreeEvaluatorTest.java
@@ -1,20 +1,22 @@
 package ro.contezi.tennis.set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 import org.junit.Test;
 
 public class SetTreeEvaluatorTest {
 
     @Test
-    public void deuceValueIsHalfWhenProbabilitiesEqualToHalf() {
-        assertThat(new SetTreeEvaluator(0.5, 0.5).computeDeuceValue()).isEqualTo(0.5);
+    public void deuceValueIsHalfWhenProbabilitiesEqual() {
+        assertThat(new SetTreeEvaluator(0.61, 0.61).computeDeuceValue()).isCloseTo(0.5, offset(0.00001));
     }
     
     @Test
     public void deuceValueIsOneWhenFirstServerIsInfinitelyBetter() {
         assertThat(new SetTreeEvaluator(1.0, 0).computeDeuceValue()).isEqualTo(1.0);
     }
+    
     @Test
     public void deuceValueIsZeroWhenFirstReceiverIsInfinitelyBetter() {
         assertThat(new SetTreeEvaluator(1.0, 0).computeDeuceValue()).isEqualTo(1.0);
